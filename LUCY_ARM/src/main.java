a6537b487c080a328b915efeb39bb4145110fa35
GilmarCorreia <gilmarjeronimo@uol.com.br>
2020-06-05 12:06:17 -0300

    Lucy Arm

--- LUCY_ARM/src/main.java
+++ LUCY_ARM/src/main.java
@@ -4,16 +4,16 @@
  * @author Gilmar Correia
  *
  */
-import src.*;
+
  
 public class main{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		Bioloid Lucy = new Bioloid(18);
 		
-		Lucy.move(2,819);
+		Lucy.move(2, 819);
 		Lucy.move(4,808);
-		Lucy.move(6,57);
+		Lucy.move(6, 57);
 		//TouchSensorSerial TS8 = new TouchSensorSerial();
 		//FuzzyProperties fuzzyCl = new FuzzyProperties();
 		//new SwingProgressBar(fuzzyCl,TS8);
